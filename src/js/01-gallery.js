9f48830213dddadf75b86c2783b742862169e2b6
OleksiiKonovalchuk <108429819+OleksiiKonovalchuk@users.noreply.github.com>
2022-11-19 03:15:57 +0200

    1-2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+function galleryCreator(gallery) {
+  const galleryToAdd = [];
+  gallery.map(({ preview, original, description }) => {
+    const image = `
+		<a class="gallery__item" href="${original}"> 
+		  <img class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+		</a>
+		`;
+    galleryToAdd.push(image);
+  });
+
+  return refs.gallery.insertAdjacentHTML('afterbegin', galleryToAdd.join(''));
+}
+galleryCreator(galleryItems);
+
+refs.gallery.addEventListener('click', onGalleryClick);
+function onGalleryClick(e) {
+  e.preventDefault();
+}
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  swipeClose: false,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
 
+const throttle = require('lodash.throttle');
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const load = () => {
+  if (localStorage.getItem('videoplayer-current-time')) {
+    return player.setCurrentTime(
+      localStorage.getItem('videoplayer-current-time')
+    );
+  }
+  return;
+};
+load();
+
+const timeCode = data => {
+  const timer = data.seconds;
+  localStorage.setItem('videoplayer-current-time', timer);
+  console.log(localStorage);
+};
+player.on('timeupdate', throttle(timeCode, 1000));
